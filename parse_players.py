71b84861edc18d2267aa6adb8ae73d0c5bc28c34
rwbogl <robert.w.bliss@gmail.com>
2017-09-09 23:24:13 -0400

    Add date parsing to parse_players.py

--- parse_players.py
+++ parse_players.py
@@ -34,6 +34,12 @@ if __name__ == "__main__":
         years = glob(path.join("./player_pages/", player_name, "*"))
 
         for year_path in years:
+            # For convenience, grab the year of the start of the season.
+            # (Fortunately, I don't think the season ever goes beyond this...)
+            year_name = path.basename(year_path)
+            stop = year_name.find("-")
+            year_name = year_name[:stop]
+
             with open(year_path) as f:
                 year_text = f.read()
 
@@ -43,28 +49,47 @@ if __name__ == "__main__":
             game_table = soup.find_all("table")[2]
             game_rows = game_table.find_all("tr")
 
+            # Occasionally, there is a final element of the table that doesn't
+            # include any stats -- usually a footnote. To detect this, we'll
+            # remove any table row that has only one <td>. This isn't a very
+            # rigorous check.
+
+            row_check = lambda r: (len(r.find_all("td")) > 1 or
+                                    len(r.find_all("th")) > 1)
+            game_rows = [r for r in game_rows if row_check(r)]
+
             year_stats = []
 
             # The first row is the header, so those are wrapped in <th></th>
             # tags. We want that information, but we have to get it separately.
 
-            # The first three elements are useless for us (opponent, date,
-            # result), so only grab the remaining ones.
-            headers = game_rows[0].find_all("th")[3:]
-            header = [h.text for h in headers]
+            # The first three elements are date, opponent, and result. The
+            # remaining ones are stats. We want the date and states, so do some
+            # work to strip the other two.
+            headers = game_rows[0].find_all("th")
+            header = [h.text for h in [headers[0]] + headers[3:]]
             year_stats.append(header)
 
             # Now get the game stats.
             for row in game_rows[1:]:
                 # The first element is the date, the second the opponent, and
-                # the third the result. We only care about the fourth and on,
-                # where the stats are.
-                stats = row.find_all("td")[3:]
+                # the third the result. We only want the date and stats, so
+                # there is some extra work to grab the date.
+                stats = row.find_all("td")
+                date_tag = stats[0]
+                stats = stats[3:]
 
                 # "Empty" stats should be left empty, not given meaningless
                 # symbols. At OU, the empty stats are marked with "-", so we'll
                 # make those blank.
                 stats = [s.text if s.text != "-" else "" for s in stats]
+
+                # Add the date back in along with the year.
+                # Date fixes: There are whitespaces and sometimes "#"s to
+                # denote footmarks. This requires some complicated stripping.
+                date = year_name + " " + date_tag.text.strip().strip("#").strip()
+                stats = [date] + stats
+
                 year_stats.append(stats)
 
             # Strip the ".html" from the year filename, then replace it with
